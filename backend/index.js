fc4d4e45fdb61619803caf6358c518ecc23a61e3
avdhesh-15 <avdhesh.03@outlook.com>
2025-08-19 20:17:25 +0530

    Production

--- backend/index.js
+++ backend/index.js
@@ -7,33 +7,34 @@ const cors = require("cors");
 const app = express();
 dotenv.config();
 
+app.use(cors({
+  origin: "https://paytm-mern-front.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
-app.use(cors());
 
 app.use("/api/v1", mainRouter);
-
-// /api/v1/user/signup
-// /api/v1/user/signin
-// /api/v1/user/changePassword.....
-
-// /api/v1/account/transferMoney
-// /api/v1/account/balance
-
-//database
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("database is connected successfully!");
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 app.get("/", (req, res) => {
   res.json("Server is up and running");
 });
 
-app.listen(process.env.PORT, () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(" Database connected successfully!");
+  } catch (err) {
+    console.error("Database connection failed", err);
+  }
+};
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port: " + process.env.PORT);
+  console.log(` Server is running on port: ${PORT}`);
 });
